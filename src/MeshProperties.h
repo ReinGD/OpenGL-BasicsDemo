1a977f0ecb4932fd6424ea017cca23c25734953d
ReinGD <37196495+JGDominik@users.noreply.github.com>
2022-11-22 15:53:10 -0500

    Nothing important

--- src/MeshProperties.h
+++ src/MeshProperties.h
@@ -62,28 +62,28 @@ public:
 
 	};
 
-	struct normals 
+	struct normal 
 	{
 		float x;
 		float y;
 		float z;
 
-		normals() = default;
+		normal() = default;
 
-		normals(const float in_x, const float in_y, const float in_z)
+		normal(const float in_x, const float in_y, const float in_z)
 		{
 			x = in_x;
 			y = in_y;
 			z = in_z;
 		};
 
-		normals(const normals& norm)
+		normal(const normal& norm)
 		{
 			this->x = norm.x;
 			this->y = norm.y;
 			this->z = norm.z;
 		};
-		normals& operator=(const normals& norm)
+		normal& operator=(const normal& norm)
 		{
 			this->x = norm.x;
 			this->y = norm.y;
@@ -92,27 +92,62 @@ public:
 
 	};
 
+	struct color
+	{
+		float r;
+		float g;
+		float b;
+		float a;
+
+		color() = default;
+
+		color(const float in_r, const float in_g, const float in_b, const float in_a)
+		{
+			r = in_r;
+			g = in_g;
+			b = in_b;
+			a = in_a;
+		};
+
+		color(const color& color)
+		{
+			this->r = color.r;
+			this->g = color.g;
+			this->b = color.b;
+			this->a = color.a;
+		};
+
+		color& operator= (const color& color)
+		{
+			this->r = color.r;
+			this->g = color.g;
+			this->b = color.b;
+			this->a = color.a;
+		};
+	};
 	struct vertex 
 	{
-		position pos;
-		textureUV uv;
-		normals norm;
+		position	pos;
+		textureUV	uv;
+		normal		norm;
+		color		col;
 
 		vertex() 
-			:pos(),uv(),norm()
+			:pos(),uv(),norm(), col()
 		{};
 
-		vertex(float x, float y, float z, float u, float v, float nx, float ny, float nz) 
-			:pos(x,y,z),uv(u,v),norm(nx,ny,nz)
+		vertex(float x, float y, float z, float u, float v, float nx, float ny, float nz, float r, float g, float b, float a) 
+			:pos(x,y,z),uv(u,v),norm(nx,ny,nz), col(r,g,b,a)
 		{
 			
 		};
-		vertex(const position in_pos, const textureUV in_uv, const normals in_normals)
-			:pos(), uv(), norm()
+		vertex(const position in_pos, const textureUV in_uv, const normal in_normals, const color in_color)
+			:pos(), uv(), norm(), col()
 		{
 			pos = in_pos;
 			uv = in_uv;
 			norm = in_normals;
+			col = in_color;
 		};
 	};
 
